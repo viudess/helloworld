038e86a02b6d4e8f5f3e95ee905c06e874dfa0f1
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-22 09:41:12 -0300

    calcTrab

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  n1 = '';
+  res = 0;
+
+  ac = '';
+  agh = '';
+  at = '';
+  nota = 0;
+
+  somar(){
+    this.res = parseFloat(this.n1) * 4.94;
+  }
+
+  calcNota(){
+    this.nota = parseFloat(this.ac) * 0.2 + parseFloat(this.agh) * 0.1 + parseFloat(this.at)  * 0.7
+  }
 
 }
+
